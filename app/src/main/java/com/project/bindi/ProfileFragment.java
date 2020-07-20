372042961e4d5f775732017282295cab5aee9f27
AnanTripathi046 <2018ugee052@nitjsr.ac.in>
2020-07-20 13:10:53 +0530

    edit profile added

--- app/src/main/java/com/project/bindi/ProfileFragment.java
+++ app/src/main/java/com/project/bindi/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.project.bindi;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class ProfileFragment extends Fragment {
-
+FloatingActionButton editFab;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view=inflater.inflate(R.layout.fragment_profile, container, false);
+        editFab=view.findViewById(R.id.editFab);
+        editFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),UpdateUserActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
